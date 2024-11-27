1c87b83ab32f36bff853f332020bc7781481a202
javi-r-p <7460103@alu.murciaeduca.es>
2024-11-27 17:11:33 +0100

    Script logm en desarrollo

--- logm.sh
+++ logm.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 echo "Monitor del sistema"
-
-if [ "$EUID" != 0 ]; then
-	echo "El script debe ser ejecutado como root."
-	exit 1
-fi
+mkdir -p /var/logm/
 
 find /var/log/logm.log > /dev/null 2> /dev/null
 if [ "$?" = "1" ]; then
-	touch /var/log/logm.log
+	touch /var/logm/logm.log
 fi
+
