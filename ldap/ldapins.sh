e9f8c26201bfec0c411b667e568dce48e5dbc647
javi-r-p <7460103@alu.murciaeduca.es>
2024-12-30 23:56:49 +0000

    Script de instalación del servidor de OpenLDAP en desarrollo

--- ldap/ldapins.sh
+++ ldap/ldapins.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+if [ $EUID -ne 0 ]; then
+	echo "El script debe ser ejecutado como root"
+	exit 1
+fi
+
 echo "Instalación del servicio de OpenLDAP y NFS"
+
+apt update > /dev/null 2> /dev/null
+echo "Instalando OpenLDAP"
+apt install slapd ldap-utils -y > /dev/null 2> /dev/null
+echo "Instalando NFS"
+apt install nfs-server -y > /dev/null 2> /dev/null
