08179471c4ae63a9992b3018cbf66124ffc43fc4
javi-r-p <7460103@alu.murciaeduca.es>
2025-02-05 18:15:43 +0100

    Lógica del programa en desarrollo, y controles de errores

--- pythonR&P/printm.py
+++ pythonR&P/printm.py
@@ -6,40 +6,45 @@ import win32printing
 # Detección del sistema operativo
 
 
-# Menú de opciones
-print("1. Ver listado de impresoras")
-print("2. Consultar la cola de impresión y/o cancelar un trabajo")
-print("3. Imprimir un documento")
-print("4. Salir")
-opcion = int(input("Selecciona una opción: "))
 
 # Control de errores
 def errores (codigo):
     if codigo == 0:
-        print("Programa finalizado exitosamente (" + codigo + ")")
+        print("Programa finalizado exitosamente (" + str(codigo) + ")")
+        exit(codigo)
+    elif codigo == 1:
+        print("Ha habido un error general (" + str(codigo) + ")")
         exit(codigo)
 
-# Opción 1: listado de impresoras
-if opcion == 1:
-    print("Has elegido ver el listado de impresoras")
-    print("Listado de impresoras:")
+opcion = 0
+def menu ():
+    # Menú de opciones
+    print("1. Ver listado de impresoras")
+    print("2. Consultar la cola de impresión y/o cancelar un trabajo")
+    print("3. Imprimir un documento")
+    print("4. Salir")
+    opcion = int(input("Selecciona una opción: "))
 
+    # Opción 1: listado de impresoras
+    if opcion == 1:
+        print("Has elegido ver el listado de impresoras")
+        print("Listado de impresoras:")
 
-    print("Tu impresora predeterminada es ")
-# Opción 2: consultar la cola y / o cancelar trabajos
-elif opcion == 2:
-    print("Has elegido ver la cola de impresión")
+        print("Tu impresora predeterminada es ")
+    # Opción 2: consultar la cola y / o cancelar trabajos
+    elif opcion == 2:
+        print("Has elegido ver la cola de impresión")
 
-    cancelar = input("¿Quieres cancelar algún trabajo? s/N")
-    if not cancelar:
-        cancelar = "n"
-    elif cancelar == "s" or cancelar == "S":
-        trabajoACancelar = int(input("Introduce el número del trabajo que quieres cancelar: "))
-
-# Opción 3: imprimir un documento
-elif opcion == 3:
-    print("Has elegido imprimir un documento")
-
-# Opción 4: salir del programa
-elif opcion == 4:
-    errores(0)
+        cancelar = input("¿Quieres cancelar algún trabajo? s/N")
+        if not cancelar:
+            cancelar = "n"
+        elif cancelar == "s" or cancelar == "S":
+            trabajoACancelar = int(input("Introduce el número del trabajo que quieres cancelar: "))
+    # Opción 3: imprimir un documento
+    elif opcion == 3:
+        print("Has elegido imprimir un documento")
+    # Opción 4: salir del programa
+    elif opcion == 4:
+        errores(0)
+    else:
+        print("No has seleccionado una opción válida")
