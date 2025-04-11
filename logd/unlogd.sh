8c47d592bb7660cfd5f68fc3cb05afb70e43bfbe
Javier Rodríguez Parra <117043352+javi-r-p@users.noreply.github.com>
2025-04-11 10:31:14 +0200

    Translate service uninstallation script to english

--- logd/unlogd.sh
+++ logd/unlogd.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
-#Se comprueba que quien ejecuta el script es el usuario root
+#Check the script is ran as root
 if [ $EUID -ne 0 ]; then
-	echo "El script debe ser ejecutado como root"
+	echo "This script must be run as root"
 	exit 1
 fi
 
-#Mensaje de bienvenida
-echo "Desinstalación del servicio logd"
+#Welcome message
+echo "Logd service uninstallation"
+echo "Logs created with this service won't be deleted"
 
-#Confirmación de desinstalación
+#Confirm uninstallation
 while
-	echo "Los logs creados hasta ahora con este servicio también serán eliminados"
-	read -p "¿Seguro que deseas desinstalar logd? (s/n) " opcion
-	[ -z $opcion ]
+	read -p "Do you really want to uninstall logd? (y/n) " option
+	[ -z $option ]
 do
-	echo "Selecciona una opción válida"
+	echo "Select a valid option"
 done
-if [ "$opcion" = "s" ]; then
-	#Desinstalación
+if [ "$option" = "s" ]; then
+	#Uninstallation
 	systemctl stop logd.service > /dev/null 2> /dev/null
-	rm -r /etc/systemd/system/logd.service /usr/bin/logd.sh /etc/logd /etc/msmtprc /var/log/logd.log > /dev/null 2> /dev/null
+	rm -r /etc/systemd/system/logd.service /usr/bin/logd.sh /etc/logd /etc/msmtprc > /dev/null 2> /dev/null
 	apt remove --purge msmtp logcheck logger -y > /dev/null 2> /dev/null
 	apt autoremove -y > /dev/null 2> /dev/null
 	systemctl daemon-reload > /dev/null 2> /dev/null
-	echo "El servicio logd se ha desinstalado. ¡Hasta pronto!"
+	echo "The service has been uninstalled. Bye!"
 	exit 0
 else
-	echo "No se ha desinstalado logd. ¡Hasta pronto!"
+	echo "The service hasn't been installed. Bye!"
 	exit 0
 fi
